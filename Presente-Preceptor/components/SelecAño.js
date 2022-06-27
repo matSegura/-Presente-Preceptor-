828a609b3a7dd18d18ebdcd069f88cab298a399b
Seba <sebaguilar03@gmail.com>
2022-06-27 17:52:17 -0300

    Update SelecAño.js

---
+++ Presente-Preceptor/components/SelecAño.js
@@ -0,0 +1,18 @@
+import React from "react-native";
+import { Text, Button, View } from "react-native";
+
+export default function SelecAño{
+    return(
+        <View>
+         <Text placeholder="Seleccione un año"/>
+         <Button placeholder="1°"/>
+         <Button placeholder="2°"/>
+         <Button placeholder="3°"/>
+         <Button placeholder="4°"/>
+         <Button placeholder="5°"/>
+         <Button placeholder="6°"/>
+         <Button placeholder="atras"/>
+         <Button placeholder="menu"/>
+        </View>
+    )
+};
