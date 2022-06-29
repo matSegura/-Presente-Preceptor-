d1172031930a58db2c6c7c1b55354a424d927c97
Seba <sebaguilar03@gmail.com>
2022-06-29 19:45:39 -0300

    Update SelecTurno.js

---
+++ Presente-Preceptor/components/SelecTurno.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Text, Button, View } from "react-native";
+
+export default function SelecTurno{
+    return(
+      <View>
+      <Text placerholder="En que turno esta el taller"/>
+      <Button placerholder="Turno mañana"/>  
+      </View>
+    )
+};
