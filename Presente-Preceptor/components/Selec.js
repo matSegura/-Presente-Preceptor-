a6f8dc2545b6985e879f123717a010f43ee185a1
matSegura <103236545+matSegura@users.noreply.github.com>
2022-06-29 19:29:59 -0300

    Comp_Teoria-Taller

--- Presente-Preceptor/components/Selec.js
+++ Presente-Preceptor/components/Selec.js
@@ -1,1 +1,11 @@
 import React from "react-native"
+import {Text, Button, View}
+
+export default Function Selec{
+    return(
+        <View>
+        <Button placerholder="Teoria"/>
+        <Button placerholder="Taller"/>
+        </View>
+    )
+} 
