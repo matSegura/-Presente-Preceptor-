88a16dc61337fcef8dd86f93c4fc54d0084d65f4
Seba <sebaguilar03@gmail.com>
2022-06-29 19:30:40 -0300

    Create SelecCurso.js

---
+++ Presente-Preceptor/components/SelecCurso.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Text, View, Button}  from "react-native";
+
+export default function SelecAño{
+    return(
+        <View>
+         <Text placeholder="Seleccione un año"/>
+         <Button placeholder="..."/>
+         <Button placeholder="..."/>
+         <Button placeholder="..."/>
+         <Button placeholder="..."/>
+         <Button placeholder="..."/>
+         <Button placeholder="..."/>
+         <Button/>
+         <Button/>
+        </View>
+    )
+};
