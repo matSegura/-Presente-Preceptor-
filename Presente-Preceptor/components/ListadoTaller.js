609081119d733cec9125f5e6860547309c2ed8f4
matSegura <103236545+matSegura@users.noreply.github.com>
2022-06-29 19:55:27 -0300

    listado de taller

--- Presente-Preceptor/components/ListadoTaller.js
+++ Presente-Preceptor/components/ListadoTaller.js
@@ -3,6 +3,16 @@ import {Text, Button,View,} from "react-native"
 
 export default Function ListadoCurs{
     return(
-       
+        <View>
+        <Text placeholder="Listado de taller"/>
+        <Text placeholder="Nombre del taller"/>
+        <Text placeholder="Nombre del Alumno"/>
+        <Button placeholder="A"/>
+        <Button placeholder="P"/>
+        <Button placeholder="T"/>
+        <Button/>
+        <Button/>
+        <Button/>
+        </View>
     )
 }
