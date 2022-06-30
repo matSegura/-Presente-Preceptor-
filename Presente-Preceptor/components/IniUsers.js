b6bf333f44749e5fc6401970949c5fe6339a20ad
EliasNun <elias10.000@live.com>
2022-06-30 11:06:55 -0300

    Update IniUsers.js

--- Presente-Preceptor/components/IniUsers.js
+++ Presente-Preceptor/components/IniUsers.js
@@ -4,8 +4,8 @@ import { Text, TextInput, View, Button } from "react-native";
 export default function IniUsers(){
     return(
         <View>
-        <Text placerholder="Inicio de usuario"/>
-        <Text placerholder="Nombre de usuario"/>
+        <Text>Inicio de usuario</Text>
+        <Text>Nombre de usuario</Text>
         <TextInput/>
         <Text placerholder="Contraseña"/>
         <TextInput/>
