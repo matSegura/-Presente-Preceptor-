acf3a44a0698554e5f4a01c8be3d2753f51610ed
EliasNun <elias10.000@live.com>
2022-06-30 10:29:46 -0300

    Correccion ListadoCurso.js

---
+++ Presente-Preceptor/components/ListadoCurso.js
@@ -0,0 +1,19 @@
+import React from "react" 
+import {Text, Button,View,} from "react-native"
+
+export default function ListadoCurso (){
+    return(
+        <View>
+        <Text> Curso {Curso}</Text>  
+        <Text> Nombre del preceptor {Preceptor} </Text> 
+        <Text> Turno {Turno}</Text> 
+        <Text> Nombre del Alumno {Alumno}</Text> 
+        <Text> A </Text> 
+        <Text> p </Text> 
+        <Text> T </Text> 
+        <Button title=""/>
+        <Button title=""/>
+        <Button title=""/>
+        </View>
+    )
+}
