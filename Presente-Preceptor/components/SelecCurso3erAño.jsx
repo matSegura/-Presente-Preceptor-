49d632d439b9ec1bc49da8b7a88c3eff7129b6b0
EliasNun <elias10.000@live.com>
2022-06-30 10:58:38 -0300

    Seleccion 3er año

---
+++ Presente-Preceptor/components/SelecCurso3erAño.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Text, View, Button}  from "react-native";
+
+export default function SelecAño3() {
+    return(
+        <View>
+         <Text>Diviciones de curso</Text>
+         <Button placeholder="3°1"/>
+         <Button placeholder="3°2"/>
+         <Button placeholder="3°3"/>
+         <Button placeholder="3°4"/>
+         <Button placeholder="3°5"/>
+         <Button placeholder="3°6"/>
+         <Button/>
+         <Button/>
+        </View>
+    )
+}
