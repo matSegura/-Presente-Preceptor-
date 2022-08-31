7929060a4d6a0092068ed2c9834b8a301a7d9788
matSegura <103236545+matSegura@users.noreply.github.com>
2022-08-30 21:43:41 -0300

    Creación de prop de ListadoCurso.

---
+++ Presente-Preceptor/Props/ListadoCurso.js
@@ -0,0 +1,8 @@
+<my component 
+ prop2="ListadoCurso.js"
+ />
+
+ function ListadoCurso ({prop2}){
+    return <Text>{prop2}</Text>,
+    <Button>{prop2}</Button>;
+ }
--- Presente-Preceptor/components/ListadoCurso.js
+++ Presente-Preceptor/components/ListadoCurso.js
@@ -6,16 +6,16 @@ import {Text, Button,View} from "react-native"
 export default function ListadoCurso (){
     return(
         <View>
-        <Text> Curso {Curso}</Text>  
-        <Text> Nombre del preceptor {Preceptor} </Text> 
-        <Text> Turno {Turno}</Text> 
-        <Text> Nombre del Alumno {Alumno}</Text> 
-        <Text> A </Text> 
-        <Text> p </Text> 
-        <Text> T </Text> 
-        <Button title=""/>
-        <Button title=""/>
-        <Button title=""/>
+            <Text> Curso {Curso}</Text>  
+            <Text> Nombre del preceptor {Preceptor} </Text> 
+            <Text> Turno {Turno}</Text> 
+            <Text> Nombre del Alumno {Alumno}</Text> 
+            <Text> A </Text> 
+            <Text> p </Text> 
+            <Text> T </Text> 
+            <Button title=""/>
+            <Button title=""/>
+            <Button title=""/>
         </View>
     )
 }
